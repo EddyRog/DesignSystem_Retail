c5f4d57048ba22717b86f7fe922d65929b2e347a
Eddy.R <pro.eddy.r@gmail.com>
2022-09-07 11:58:58 +0200

    Init Project

---
+++ DesignSystemRetail/DesignSystemRetail/DesignSystemRetailApp.swift
@@ -0,0 +1,17 @@
+//
+// DesignSystemRetailApp.swift
+// DesignSystemRetail
+// Created in 2022
+// Swift 5.0
+
+
+import SwiftUI
+
+@main
+struct DesignSystemRetailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
